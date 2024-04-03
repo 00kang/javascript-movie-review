92496517c23437d166839439af5cefd1ecc561e4
00kang <kdb070970@gmail.com>
2024-04-03 20:03:11 +0900

    feat: output 경로 수정 및 재빌드

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     historyApiFallback: true,
   },
   output: {
-    // publicPath: '/',
+    publicPath: '/dist/',
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
